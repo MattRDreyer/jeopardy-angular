28b5d5c2391510fa64e78f670495ac8e4cb3c42f
Christopher Woods <Christopher.Woods@LibertyMutual.com>
2017-06-27 15:17:40 -0400

    Add Status Message Box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { StatusMessageComponent } from './status-message/status-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    StatusMessageComponent
   ],
   imports: [
     BrowserModule
